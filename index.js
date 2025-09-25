463e4e40da718bf8b773e6b1bb8861986e47d4f0
ronniegikandi <kiiruronnie42@gmil.com>
2025-09-25 16:56:16 +0300

    dhd

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  const newCats = [...cats];
+  newCats.pop();
+  return newCats;
+}
+
+function removeFirstCat() {
+  const newCats = [...cats];
+  newCats.shift();
+  return newCats;
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat
+};
